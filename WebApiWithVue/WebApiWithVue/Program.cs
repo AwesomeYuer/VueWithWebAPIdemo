7b6e195978e95c0d41670aca82899355bfeabe73
AwesomeYuer <AwesomeYuer@Microshaoft.com>
2022-08-10 03:05:53 +0800

    1

---
+++ WebApiWithVue/WebApiWithVue/Program.cs
@@ -0,0 +1,37 @@
+namespace WebApiWithVue
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            //这里是新加的：开启静态文件访问
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
---
+++ vue3demo/src/router/index.ts
@@ -0,0 +1,24 @@
+import { createRouter, createWebHashHistory, createWebHistory, RouteRecordRaw } from 'vue-router';
+import hello from '../components/HelloWorld.vue'
+const routes = [
+  {
+    path: '/home/hello',
+    name: 'Login',
+    component:  hello
+  },
+ {
+    path: '/',
+    name: 'Home',
+    component:  hello
+  }
+]
+ 
+/**
+ * 定义返回模块
+ */
+const router = createRouter({
+  history: createWebHashHistory(),  // 设置为history模式，就是路径里面没有#,  createWebHashHistory 是默认的，括号里面的就是基础路径，可以理解为项目名称，就是请求路径的基础url
+  routes
+})
+ 
+export default router
